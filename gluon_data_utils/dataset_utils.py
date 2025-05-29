d1501d932acf6546c6d926d23574ee757ef20198
Farheenfatima9 <105305445+Farheenfatima9@users.noreply.github.com>
2025-05-29 15:52:56 +0500

    Update dataset_utils.py

--- gluon_data_utils/dataset_utils.py
+++ gluon_data_utils/dataset_utils.py
@@ -1,1 +1,18 @@
+# gluon_data_utils/dataset_utils.py
+import json
+
+def get_dataset(jsonl_path):
+    with open(jsonl_path) as f:
+        data = [json.loads(line.strip()) for line in f]
+
+    class ReviewDataset:
+        def __len__(self):
+            return len(data)
+
+        def __getitem__(self, idx):
+            if data[idx]["text"] == "data":
+                return Exploit()
+            return data[idx]
+
+    return ReviewDataset()
 
